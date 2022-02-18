dd8e204eef0b27c317992c9091e2f05888ef82ce
user <yesha.maryloui@gmail.com>
2022-02-19 02:16:12 +0800

    Update File

--- Assignment 10(1).py
+++ Assignment 10(1).py
@@ -13,21 +13,21 @@ def qrcode(frame):
         cv2.putText(frame, "CONTACT TRACING", (z + 6, y - 6), font, 1.0, (0,0,0), 1)
         
         with open ("qrdata.txt", mode='w') as file:
-            file.write("Recognized Code")
+            file.write(qrcode_info)
 
     return frame
 
-def qrfinal():
-    reader=cv2.VideoCapture(0)
-    ret, frame=reader.read()
-    
-    while ret:
-        ret, frame=reader.read()
-        frame = qrcode(frame)
-        cv2.inshow('QR reader', frame)
-        if cv2.waitKey(1) & 0xFF==27:
-            break
-    reader.release()
-    cv2.destroyAllWindows()
 
-qrfinal()
+reader=cv2.VideoCapture(0)
+ret, frame=reader.read()
+    
+while ret:
+    ret, frame=reader.read()
+    frame = qrcode(frame)
+    cv2.imshow('QR reader', frame)
+    if cv2.waitKey(1) & 0xFF==27:
+        break
+reader.release()
+cv2.destroyAllWindows()
+
+
--- qrcode1.py
+++ qrcode1.py
@@ -1,18 +1,18 @@
 import cv2
-import webbrowser
 
 cap=cv2.VideoCapture(0)
 detector=cv2.QRCodeDetector()
+
 while True:
     _, img=cap.read()
     data,one,_=detector.detectAndDecode(img)
     if data:
         a=data
         break
-    cv2.imshow('QR READER')
+    cv2.imshow('QR READER', img)
     if cv2.waitKey(1)==ord('q'):
         break
-b=webbrowser.open(str(a))
+
 cap.release(a)
 cv2.destroyAllWindows()
 
