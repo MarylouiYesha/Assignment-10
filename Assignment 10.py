ae0a54bf457c15ec4f91c5de3cd85ea3ede7635e
user <yesha.maryloui@gmail.com>
2022-02-19 00:26:04 +0800

    Upload Files

--- Assignment 10.py
+++ Assignment 10.py
@@ -1,17 +1,29 @@
 import cv2
+from pyzbar import pyzbar
 
+def qrcode(frame):
+    qrcodes = pyzbar.decode(frame)
+    for qrcode in qrcodes:
+        z, y , x, w = qrcode.rect
+
+        qrcode_info=qrcode.data.decode('utf-8')
+        cv2.square(frame, (z, y)), (z+x, y+w), (0, 255, 0), 2)
+
+        font=cv2.FONT_HERSHEY_DUPLEX
+        
+        with open ("qrdata.txt", mode='w') as file:
+            file.write("Recognized Code")
+
+    return frame
 
 reader=cv2.VideoCapture(0)
-qrdetector=cv2.QRCodeDetector()
-while True:
-    _,img=reader.read()
-    data,one,_=qrdetector.detectAndDecode(img)
-    if data:
-        a=data
-        break 
-    cv2.imshow('qrscanner',img)
-    if cv2.waitKey(1)==ord('q'):
-        break
+ret, frame=reader.read()
 
+while ret:
+    ret, frame=reader.read()
+    frame = qrcode(frame)
+    cv2.imdow('QR reader', frame)
+    if cv2.waitKey(1) & 0xFF==27:
+        break
 reader.release()
-cv2.destroyAllWindows
+cv2.destroyAllWindows()
