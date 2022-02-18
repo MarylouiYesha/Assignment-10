4ea9439dd97cc11092e66b24972e3a6cdbf42157
user <yesha.maryloui@gmail.com>
2022-02-18 16:15:13 +0800

    Updating

--- Assignment 10.py
+++ Assignment 10.py
@@ -3,3 +3,16 @@ import webbrowser
 
 reader=cv2.VideoCapture(0)
 qrdetect=cv2.QRCodeDetector()
+
+while True:
+    _, img=cap.read()
+    data,one, _=detector.detectAndDecode(img)
+    if data:
+        a=data
+        break
+    cv2.imshow('QRCODEscanner', img)
+    if cv2.waitKey(1)== ord('q')
+    break
+b=webbrowser.open(str(a))
+cap.release(a)
+cv2.destroyAllWindows
